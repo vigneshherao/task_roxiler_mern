cbb8de081dee340c4931dc2e87c7c3359e14ee57
vignesh <vigneshfornavy@gmail.com>
2024-03-24 16:28:39 +0530

    package

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -12,7 +12,7 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const response = await fetch("https://task-roxiler-mern-vignesh.onrender.com/list");
+    const response = await fetch("/list");
     const datajson = await response.json();
     setInfo(datajson);
   };
