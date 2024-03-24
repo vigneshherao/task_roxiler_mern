57e77229bb1389df480cedb0c992d1a0b4a50ca6
vignesh <vigneshfornavy@gmail.com>
2024-03-24 13:41:49 +0530

    Added Env File for Backend

--- server/index.js
+++ server/index.js
@@ -3,11 +3,19 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 const RoxilerData = require("./models/roxilerModel");
+const cors = require('cors')
+require('dotenv').config()
 
+app.use(cors())
+
+
+const dbUrl = process.env.ATLASDBURL;
 main().then(()=>console.log("db connected")).catch(err => console.log(err));
 
+
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/roxiler');
+
+  await mongoose.connect(dbUrl);
 
 }
 
